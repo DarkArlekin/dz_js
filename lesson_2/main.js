43535aa15f8511a252bf0efae37b45d0152bdede
lllena <lena.lutchencko@gmail.com>
2021-02-14 21:13:13 +0300

    add lesson_2

---
+++ lesson_2/main.js
@@ -0,0 +1,14 @@
+let money = 54000;
+let income = 20000;
+let addExpenses = 'Интеренет, такси, телефон';
+let deposit = true;
+let mission = 500000000000;
+let period = 3;
+
+console.log(typeof(money) +' '+ typeof(income) + ' '+typeof(deposit));
+console.log('Период равен '+period+' месяцев.');
+console.log('Цель заработать '+mission+' долларов');
+console.log(addExpenses.toLowerCase().split(', '))
+let butgetDay=money/30;
+console.log(butgetDay);
+
