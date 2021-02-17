7871ce6b982abc819b8e11cac5ca007400ba6622
lllena <lena.lutchencko@gmail.com>
2021-02-18 02:29:19 +0300

    add dop

--- lesson_5-1/main.js
+++ lesson_5-1/main.js
@@ -4,3 +4,24 @@ arr.forEach((element) => {
     console.log(element);
   }
 });
+console.log(`\n\n\n`);
+
+let del=2;
+for(let i=1; i<101; i++){
+  while(del<=100){
+  if(i%del==0){
+    break;
+  }
+  else{
+    del++;
+  }}
+  if(i==del){
+    console.log(del)
+    console.log(`Делители этого числа: 1 и ${del}`)
+  }
+ del=2;
+}
+
+
+
+
