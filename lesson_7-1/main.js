2aa9d5238078e0d45cc5c68a74fdb5d2e05a51c4
lllena <lena.lutchencko@gmail.com>
2021-02-24 13:39:11 +0300

    add lesson 7-1

--- lesson_7-1/main.js
+++ lesson_7-1/main.js
@@ -1,24 +1,18 @@
 'use strict';
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const day = document.getElementById('fordays'); // Получаем div куда всё будем вставлять
-const todayDay = new Date();
+let i = 0,
+  today = new Date().getUTCDay();
+week.forEach((element) => {
+  document.querySelector('.days').innerHTML += `<li class="day${i}">${element}</li>`;
+  if (i == 5 || i == 6) {
+    document.querySelector(`.day${i}`).classList.add('italic');
+    console.log(element);
+  }
+  if (i === today) {
+    document.querySelector(`.day${i - 1}`).classList.add('today');
+  }
+  i++;
+});
 
-const days = () => {
-    week.forEach((item, i) => {
-        let newdiv = document.createElement('div'); // Для каждого элемента week создаём div
-        if (i === +todayDay.getDay()-1) { // Если текущий день недели то от номера текущей даты отнимаем единицу, так как массив у нас начинается с нуля
-            console.log(todayDay.getDay());
-            newdiv.classList.add('today'); // Добавляем класс (делаем жирным)
-            newdiv.textContent = week[i]; // Вставляем текст
-        }
-        if (item == 'Суббота' || item == 'Воскресенье') { // Если выходные то
-            newdiv.classList.add('italic'); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
-            newdiv.textContent = week[i]; // Вставляем текст
-        } else {
-            newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
-        }
-        day.appendChild(newdiv); // Добавляем новый див в div @day
-
-    });
-};
-days(); // Вызываем функцию;
+console.log();
+//document.querySelector('.days').textContent = week;
