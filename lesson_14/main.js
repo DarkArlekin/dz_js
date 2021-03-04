8f319563c06e2b47d2e7b7d4125abbf2f799c758
lllena <lutchencko.lena@gmail.com>
2021-03-04 02:10:57 +0200

    add lesson_14

---
+++ lesson_14/main.js
@@ -0,0 +1,6 @@
+var div = document.createElement("div");
+div.innerHTML = 'text2';
+div.style.backgroundColor = 'red';
+div.style.width = '150px';
+div.style.height = '80px';
+document.body.appendChild(div); 
