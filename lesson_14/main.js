8c551d36c32303716b83b32ea3214451b854c340
lllena <lutchencko.lena@gmail.com>
2021-03-10 16:38:48 +0200

    add additional lesson

--- lesson_14/main.js
+++ lesson_14/main.js
@@ -16,15 +16,34 @@ class DomElement {
       elem = document.createElement('p');
       elem.id = this.selector.slice(1);
     }
+    //elem.style.cssText = `background-color: ${this.bg};height: ${this.height}; width: ${this.width}; font-size: ${this.fontSize}; position: absolute`;
     elem.style.backgroundColor = this.bg;
     elem.style.height = this.height;
     elem.style.width = this.width;
     elem.style.fontSize = this.fontSize;
-    elem.textContent = 'This 1 part lesson 14';
+    elem.style.position = 'absolute';
+    elem.style.top = '0px';
+    elem.style.bottom = '100px';
+    elem.style.left = '0px';
+    elem.style.right = '100px';
+    elem.textContent = 'This lesson 14';
     document.body.appendChild(elem);
   }
 }
-let a = new DomElement('.dgdf', '150px', '150px', 'red', '20px');
+let a = new DomElement('.dgdf', '100px', '100px', 'red', '20px');
 a.createEl();
-
-console.log(a);
+const obj = document.querySelector(`${a.selector}`);
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowUp') {
+      obj.style.top = parseInt(obj.style.top) - 10 + 'px';
+    }
+    if (event.code === 'ArrowDown') {
+      obj.style.top = parseInt(obj.style.top) + 10 + 'px';
+    }
+    if (event.code === 'ArrowLeft') {
+      obj.style.left = parseInt(obj.style.left) - 10 + 'px';
+    }
+    if (event.code === 'ArrowRight') {
+      obj.style.left = parseInt(obj.style.left) + 10 + 'px';
+    }
+  });
